c36b6f744bb45f7d508b36468c3fc18dfc12e571
Gustavo Viana de Souza <gustavovianadesouzacontatos@gmail.com>
2026-01-17 18:40:02 -0300

    dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import "dotenv/config";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
